8ce654d44924368c2bf915beb95efa641e8e6633
Clean Ts <cleants1@gmail.com>
2021-10-11 09:34:11 -0500

    refactor: elimination of test project.

--- app/README.md
+++
@@ -1,7 +0,0 @@
-## Awesome Project Build with Clean Architecture
-
-Steps to run this project:
-
-1. Run `npm i` command
-
-2. Run `npm watch` command
--- app/src/application/app.ts
+++
@@ -1,9 +0,0 @@
-import {Container} from "clean-ts";
-
-@Container({
-    imports: [],
-    controllers: [],
-    providers: []
-})
-
-export class AppContainer {}
