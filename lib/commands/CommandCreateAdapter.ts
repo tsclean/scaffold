88f6d31c3076070e98127ce8e5ea9bc69e40d8ba
Clean Ts <cleants1@gmail.com>
2021-11-09 17:07:44 -0500

    fix: adapter correction server name

--- lib/commands/CommandCreateAdapter.ts
+++ lib/commands/CommandCreateAdapter.ts
@@ -259,7 +259,7 @@ import helmet from 'helmet';
 import { Dialect } from 'sequelize/types';
 import { Sequelize } from 'sequelize-typescript';
 
-import {StartProjectServer} from "@tsclean/core";
+import {StartProjectInit} from "@tsclean/core";
         
 import {AppContainer} from "@/application/app";
 import {PORT, CONFIG_MYSQL} from "@/application/config/environment";
@@ -273,7 +273,7 @@ const sequelize = new Sequelize(CONFIG_MYSQL.database, CONFIG_MYSQL.user, CONFIG
 async function init() {
     await sequelize.authenticate()
     console.log("DB mysql")
-    const app = await StartProjectServer.create(AppContainer)
+    const app = await StartProjectInit.create(AppContainer)
     app.use(helmet());
     await app.listen(PORT, () => console.log('Running on port ' + PORT))
 }
@@ -288,7 +288,7 @@ import helmet from 'helmet';
 import { Dialect } from 'sequelize/types';
 import { Sequelize } from 'sequelize-typescript';
 
-import {StartProjectServer} from "@tsclean/core";
+import {StartProjectInit} from "@tsclean/core";
         
 import {AppContainer} from "@/application/app";
 import {PORT, CONFIG_POSTGRES} from "@/application/config/environment";
@@ -311,7 +311,7 @@ const sequelize = new Sequelize(CONFIG_POSTGRES.database, CONFIG_POSTGRES.user, 
 async function init() {
     await sequelize.authenticate()
     console.log("DB postgres")
-    const app = await StartProjectServer.create(AppContainer)
+    const app = await StartProjectInit.create(AppContainer)
     app.use(helmet());
     await app.listen(PORT, () => console.log('Running on port ' + PORT))
 }
@@ -328,7 +328,7 @@ init();`
 
 import helmet from 'helmet';
 import { connect } from 'mongoose';
-import { StartProjectServer } from "@tsclean/core";
+import { StartProjectInit } from "@tsclean/core";
 
 import { AppContainer } from "@/application/app";
 import {MONGODB_URI, PORT} from "@/application/config/environment";
@@ -336,7 +336,7 @@ import {MONGODB_URI, PORT} from "@/application/config/environment";
 async function run(): Promise<void> {
   await connect(MONGODB_URI);
   console.log('DB Mongo connected')
-  const app = await StartProjectServer.create(AppContainer);
+  const app = await StartProjectInit.create(AppContainer);
    app.use(helmet());
    await app.listen(PORT, () => console.log('Running on port: ' + PORT))
 }
