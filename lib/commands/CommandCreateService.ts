6a4ccad13969375119195a782448f596a87a8c03
Over Group <116088843+Overgroup1@users.noreply.github.com>
2023-11-01 21:38:09 +0100

    fix: change logic for generate service interface

--- lib/commands/CommandCreateService.ts
+++ lib/commands/CommandCreateService.ts
@@ -1,90 +1,97 @@
 import ora from "ora";
 import yargs from "yargs";
 
-import {EMOJIS} from "../utils/emojis";
-import {MESSAGES} from "../utils/messages";
-import {CommandUtils} from "./CommandUtils";
-import {banner, errorMessage} from "../utils/helpers";
+import { EMOJIS } from "../utils/emojis";
+import { MESSAGES } from "../utils/messages";
+import { CommandUtils } from "./CommandUtils";
+import { banner, errorMessage } from "../utils/helpers";
 
 export class ServiceCreateCommand implements yargs.CommandModule {
-    command = "create:service";
-    describe = "Generates a new service.";
+  command = "create:service";
+  describe = "Generates a new service.";
 
-    builder(args: yargs.Argv) {
-        return args
-            .option("n", {
-                alias: "name",
-                describe: "Name the Service class",
-                demandOption: true
-            })
-    };
+  builder(args: yargs.Argv) {
+    return args.option("n", {
+      alias: "name",
+      describe: "Name the Service class",
+      demandOption: true
+    });
+  }
 
-    async handler(args: yargs.Arguments) {
-        let spinner
+  async handler(args: yargs.Arguments) {
+    let spinner;
 
-        try {
+    try {
+      const fileContent = ServiceCreateCommand.getTemplateService(
+        args.name as any
+      );
+      const fileContentRepository = ServiceCreateCommand.getTemplateIServices(
+        args.name as any
+      );
 
-            const fileContent = ServiceCreateCommand.getTemplateService(args.name as any)
-            const fileContentRepository = ServiceCreateCommand.getTemplateIServices(args.name as any)
+      const basePath = `${process.cwd()}/src/domain/use-cases/`;
+      const filename = `${args.name}-service-impl.ts`;
+      const path = `${basePath}impl/${filename}`;
+      const pathRepository = `${basePath + args.name}-service.ts`;
 
-            const basePath = `${process.cwd()}/src/domain/use-cases/`;
-            const filename = `${args.name}-service-impl.ts`;
-            const path = `${basePath}impl/${filename}`;
-            const pathRepository = `${basePath + args.name}-service.ts`;
+      const fileExists = await CommandUtils.fileExists(path);
 
-            const fileExists = await CommandUtils.fileExists(path)
+      setTimeout(() => (spinner = ora("Installing...").start()), 1000);
 
-            setTimeout(() => (spinner = ora('Installing...').start()), 1000)
+      if (fileExists) throw MESSAGES.FILE_EXISTS(path);
 
-            if (fileExists) throw MESSAGES.FILE_EXISTS(path)
+      await CommandUtils.createFile(pathRepository, fileContentRepository);
+      await CommandUtils.createFile(path, fileContent);
 
-            await CommandUtils.createFile(pathRepository, fileContentRepository)
-            await CommandUtils.createFile(path, fileContent)
-
-            setTimeout(() => {
-                spinner.succeed("Installation completed")
-                spinner.stopAndPersist({
-                    symbol: EMOJIS.ROCKET,
-                    prefixText: MESSAGES.REPOSITORY_SUCCESS(pathRepository),
-                    text: MESSAGES.FILE_SUCCESS('Services', path)
-                });
-            }, 1000 * 5);
-
-        } catch (error) {
-            setTimeout(() => (spinner.fail("Installation fail"), errorMessage(error, 'service')), 2000)
-        }
+      setTimeout(() => {
+        spinner.succeed("Installation completed");
+        spinner.stopAndPersist({
+          symbol: EMOJIS.ROCKET,
+          prefixText: MESSAGES.REPOSITORY_SUCCESS(pathRepository),
+          text: MESSAGES.FILE_SUCCESS("Services", path)
+        });
+      }, 1000 * 5);
+    } catch (error) {
+      setTimeout(
+        () => (
+          spinner.fail("Installation fail"), errorMessage(error, "service")
+        ),
+        2000
+      );
     }
+  }
 
-    /**
-     * Get contents services files
-     * @param param
-     * @protected
-     */
-    protected static getTemplateService(param: any) {
-        const name = CommandUtils.capitalizeString(param)
-        return `import {Service} from "@tsclean/core";
+  /**
+   * Get contents services files
+   * @param param
+   * @protected
+   */
+  protected static getTemplateService(param: any) {
+    const name = CommandUtils.capitalizeString(param);
+    return `import {Service} from "@tsclean/core";
 import {I${name}Service} from "@/domain/use-cases/${param}-service";
 
 @Service()
 export class ${name}ServiceImpl implements I${name}Service {
     constructor() {
     }
-}`
-    }
+}`;
+  }
 
-    /**
-     * Get contents interface files
-     * @param param
-     * @protected
-     */
-    protected static getTemplateIServices(param: string) {
-        const name = CommandUtils.capitalizeString(param)
-        const nameRef = param.toUpperCase();
+  /**
+   * Get contents interface files
+   * @param param
+   * @protected
+   */
+  protected static getTemplateIServices(param: string) {
+    const name = CommandUtils.capitalizeString(param);
+    const nameTransform = param.replace(/-/g, "_");
+    const nameRef = nameTransform.toUpperCase();
 
-        return `export const ${nameRef}_SERVICES = '${nameRef}_SERVICES';
+    return `export const ${nameRef}_SERVICES = '${nameRef}_SERVICES';
         
 export interface I${name}Service {
 
-}`
-    }
+}`;
+  }
 }
